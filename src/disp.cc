3ab7630d9a448446a8b69aad8bf454be6c8b6880
ming <chen.zhang@rokid.com>
2018-09-21 16:33:35 +0800

    Revert "添加log调试dispatcher偶现卡死问题"
    This reverts commit 705ab1db0681d41917e87e762ee4e44974f7fae4.

--- src/disp.cc
+++ src/disp.cc
@@ -54,14 +54,7 @@ bool Dispatcher::put(Frame& frame, shared_ptr<Adapter>& sender) {
     KLOGE(TAG, "msg cmd invalid: %d", cmd);
     return false;
   }
-#ifdef FLORA_DEBUG2
-  KLOGI(TAG, "Dispatcher.put: before msg handler, cmd %d", cmd);
-#endif
-  bool r = (this->*(msg_handlers[cmd]))(msg_caps, sender);
-#ifdef FLORA_DEBUG2
-  KLOGI(TAG, "Dispatcher.put: after msg handler, result %d", r);
-#endif
-  return r;
+  return (this->*(msg_handlers[cmd]))(msg_caps, sender);
 }
 
 bool Dispatcher::handle_auth_req(shared_ptr<Caps>& msg_caps,
